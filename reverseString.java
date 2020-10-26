48961ee4327979771c8a08df534520c37e22b758
PushparajManickam <47779182+PushparajManickam@users.noreply.github.com>
2020-10-26 13:15:54 +0530

    Add files via upload

---
+++ reverseString.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void reverseString(char[] s) {
+        char temp;
+        for(int i=0, j=s.length-1;i<j;i++,j--)
+        {
+            temp=s[j];
+            s[j]=s[i];
+            s[i]=temp;
+        }
+    }
+}
