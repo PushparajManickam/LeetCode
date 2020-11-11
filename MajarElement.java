54791eff215874f17327a3adb8317aba0de5dce6
PushparajManickam <47779182+PushparajManickam@users.noreply.github.com>
2020-11-11 21:53:46 +0530

    Update MajarElement.java

--- MajarElement.java
+++ MajarElement.java
@@ -1,20 +1,20 @@
 class MajorElement {
     public int majorityElement(int[] nums) {
-        int temp, temp1, maxicount=nums[0], n, count=0,tempcount;
+        int loop1, loop2, temp, n, major, count=0,tempcount;
         n=nums.length;
-        for(int loop1=0;loop1<n;loop1++)
+        major = nums[0];
+        for(loop1=0;loop1<n;loop1++)
         {
             tempcount=0;
-            temp = nums[loop1];
-            for(int loop2=loop1+1;loop2<n;loop2++){
-              if(temp==nums[loop2])
+            for(loop2=loop1+1;loop2<n;loop2++){
+              if(nums[loop1]==nums[loop2])
                     tempcount++;
             }
             if(count<tempcount){
                 count = tempcount;
-                maxicount = temp;
+                major = nums[loop1];
             } 
         }
-        return maxicount;
+        return major;
     }
 }
